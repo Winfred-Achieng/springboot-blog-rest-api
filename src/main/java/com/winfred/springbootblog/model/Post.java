7f3bf239afef90b40393fb5044336c965a1df87d
Winfred-Achieng <achiengwinfred20@gmail.com>
2024-05-29 22:59:34 +0300

    comment entity

--- src/main/java/com/winfred/springbootblog/model/Post.java
+++ src/main/java/com/winfred/springbootblog/model/Post.java
@@ -5,11 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table( name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+
 public class Post {
 
     @Id
@@ -21,4 +25,8 @@ public class Post {
     private String description;
     @Column(name = "content", nullable = false)
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
+
 }
