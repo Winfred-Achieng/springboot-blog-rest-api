6e0cb2cdb9e68f485aac3b937f616a1e42e370b6
Winfred-Achieng <achiengwinfred20@gmail.com>
2024-06-17 13:33:49 +0300

    category entity

---
+++ src/main/java/com/winfred/springbootblog/model/Category.java
@@ -0,0 +1,21 @@
+package com.winfred.springbootblog.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+}
