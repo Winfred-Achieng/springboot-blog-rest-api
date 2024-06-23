e0552bdf5470cbcdf5dd312061a0fa0440f821be
Winfred-Achieng <achiengwinfred20@gmail.com>
2024-06-23 21:06:14 +0300

    Using profiles and wiriting a code to insert metadata into tables

--- src/main/java/com/winfred/springbootblog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/winfred/springbootblog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,15 @@
 package com.winfred.springbootblog;
 
+import com.winfred.springbootblog.model.Role;
+import com.winfred.springbootblog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -30,15 +34,31 @@ import org.springframework.context.annotation.Bean;
         )
 )
 
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
 
+    @Autowired
+    public RoleRepository repository;
+
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        repository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        repository.save(userRole);
+
+    }
 }
